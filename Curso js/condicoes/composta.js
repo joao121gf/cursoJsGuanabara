c9474feb8dedc53c6e7b7f6abb2d570db82fe2d6
João Giovani <giovannirs957@gmail.com>
2023-03-26 14:42:57 -0300

    Exercício de velocidade

---
+++ Curso js/condicoes/composta.js
@@ -0,0 +1,10 @@
+var pais = 'Brasil';
+
+
+
+if (pais != 'Brasil'){
+    console.log('Estrangeiro')
+}else{
+    console.log('Brasileiro')
+}
+
---
+++ Curso js/condicoes/index.js
@@ -0,0 +1,9 @@
+var vel = 60.6;
+console.log(`A velocidade do seu carro é ${vel} por hora`);
+
+if (vel > 60){
+    console.log('Voce ultrapassou a velocidade permitida')
+}
+else{
+    console.log('ta ok')
+}
